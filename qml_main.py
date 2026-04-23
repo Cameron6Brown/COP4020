6faad5bb63dfc52929745ce67ebf3c1caa26d381
Jackson Kiewiet <jackson.kiewiet@gmail.com>
2026-04-23 01:03:03 -0500

    Updated driver to allow for different file usage

--- qml_main.py
+++ qml_main.py
@@ -1,13 +1,21 @@
 """
 Main Driver File
+
+To use this transpiler, 
+format cmd line input as:
+
+python qml_main.py <filename>
 """
 
+import sys
 from qml_lexer import tokenize
 from qml_parser import parse
 from qml_serializer import Serialize
 
-source = open("midterm.quiz").read()
+
+source = open(sys.argv[1]).read()
 tokens = tokenize(source)
 ast = parse(tokens)
-output = Serialize(ast)
-print(output)
+output_file = sys.argv[1].replace(".quiz", ".json")
+Serialize(output_file, ast)
+print(f"Output written to {output_file}.")
